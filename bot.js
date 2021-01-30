4769ea5bf6e5048e63551e6f5f3d937756f3d552
MrPaschenko <paschenkod2@gmail.com>
2021-01-31 01:44:24 +0200

    bot.js

--- bot.js
+++ bot.js
@@ -1,11 +1,21 @@
 'use strict';
 
 const { Telegraf } = require('telegraf');
+const fs = require('fs');
 const config = require('config');
 const bot = new Telegraf(config.get('token'));
 
-bot.start(ctx => ctx.reply('Welcome'));
-bot.help(ctx => ctx.reply('Send me a sticker'));
-bot.on('sticker', ctx => ctx.reply('👍'));
-bot.hears('hi', ctx => ctx.reply('Hey there'));
+bot.on('message', ctx => {
+  const db = JSON.parse(fs.readFileSync('db.json').toString());
+  db[ctx.message.from.id] = `@${ctx.message.from.username}`;
+  db[ctx.message.chat.id] = ctx.message.chat.title;
+  fs.writeFileSync('db.json', JSON.stringify(db));
+});
+
+bot.on('channel_post', ctx => {
+  if (ctx.update.channel_post.sender_chat.id === -1001341697220) {
+    ctx.forwardMessage(197415735);
+  }
+});
+
 bot.launch();
