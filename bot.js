c4cbdc994a4451abfb750f3e08a9b99c2ec46215
Dmytro Pashchenko <paschenkod2@gmail.com>
2021-02-04 22:25:37 +0200

    /gordon and /wolfram commands

--- bot.js
+++ bot.js
@@ -1,13 +1,35 @@
 'use strict';
 
-const { Telegraf } = require('telegraf');
 const fs = require('fs');
 const config = require('config');
+const { Telegraf } = require('telegraf');
 const bot = new Telegraf(config.get('token'));
+const WolframAlphaAPI = require('wolfram-alpha-api');
+const waApi = WolframAlphaAPI(config.get('wolfram'));
 
-bot.on('sticker', ctx => {
-  ctx.replyWithSticker(ctx.update.message.sticker.file_id);
-})
+bot.command('gordon', ctx => {
+  const min = 1;
+  const max = fs.readdirSync('gordon').length;
+  const number = Math.round(Math.random() * (max - min) + min);
+  const path = `gordon/${number}.jpg`;
+  ctx.replyWithPhoto({ source: path });
+});
+
+bot.command('wolfram', ctx => {
+  const inputArray = ctx.message.text.split(' ');
+  inputArray.shift();
+  const input = inputArray.join(' ');
+
+  if (!input) {
+    ctx.reply('Введи запрос после команды');
+  } else {
+    waApi.getShort(input).then(input => {
+      ctx.reply(input);
+    }).catch(error => {
+      ctx.reply(error.message);
+    });
+  }
+});
 
 bot.on('message', ctx => {
   const db = JSON.parse(fs.readFileSync('db.json').toString());
